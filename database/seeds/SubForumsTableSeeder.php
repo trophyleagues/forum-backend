fcf52fa5dc4268688464c19d2283e732e9ec9eb0
gertoska <tosk1@protonmail.com>
2019-04-26 14:33:07 +0200

    Seed database with posts.

--- database/seeds/SubForumsTableSeeder.php
+++ database/seeds/SubForumsTableSeeder.php
@@ -1,14 +1,20 @@
 <?php
 
 use App\Models\Author;
+use App\Models\Post;
 use App\Models\Role;
 use App\Models\SubForum;
 use Illuminate\Database\Seeder;
+use Tests\Shared\Domain\ValueObject\ContentStub;
 use Tests\Shared\Domain\ValueObject\CreatedAtStub;
+use Tests\Shared\Domain\ValueObject\SlugStub;
+use Tests\Shared\Domain\ValueObject\TitleStub;
 use Tests\Shared\Domain\ValueObject\UpdatedAtStub;
 use Tests\TrophyForum\Authors\Domain\AuthorAvatarStub;
 use Tests\TrophyForum\Authors\Domain\AuthorIdStub;
 use Tests\TrophyForum\Authors\Domain\AuthorNameStub;
+use Tests\TrophyForum\Posts\Domain\PostIdStub;
+use Tests\TrophyForum\Posts\Domain\PostIsOpenStub;
 use Tests\TrophyForum\Roles\Domain\RoleIdStub;
 use Tests\TrophyForum\Roles\Domain\RoleNameStub;
 use Tests\TrophyForum\SubForums\Domain\SubForumDescriptionStub;
@@ -31,9 +37,11 @@ final class SubForumsTableSeeder extends Seeder
                 ]
             );
 
+            $subForumId = SubForumId::random()->value();
+
             SubForum::create(
                 [
-                    'id'          => SubForumId::random()->value(),
+                    'id'          => $subForumId,
                     'author_id'   => $authorId,
                     'name'        => SubForumNameStub::random()->value(),
                     'description' => SubForumDescriptionStub::random()->value(),
@@ -42,6 +50,32 @@ final class SubForumsTableSeeder extends Seeder
                     'updated_at'  => UpdatedAtStub::random()->value(),
                 ]
             );
+
+            for ($j = 0; $j < rand(0,10); $j++) {
+                $authorId = AuthorIdStub::random()->value();
+
+                Author::create(
+                    [
+                        'id'     => $authorId,
+                        'name'   => AuthorNameStub::random()->value(),
+                        'avatar' => AuthorAvatarStub::random()->value(),
+                    ]
+                );
+
+                Post::create(
+                    [
+                        'id'           => PostIdStub::random()->value(),
+                        'sub_forum_id' => $subForumId,
+                        'author_id'    => $authorId,
+                        'title'        => TitleStub::random()->value(),
+                        'content'      => ContentStub::random()->value(),
+                        'is_open'      => PostIsOpenStub::random()->value(),
+                        'slug'         => SlugStub::random()->value(),
+                        'created_at'   => CreatedAtStub::random()->value(),
+                        'updated_at'   => UpdatedAtStub::random()->value(),
+                    ]
+                );
+            }
 
             Role::create(
                 [
@@ -54,10 +88,12 @@ final class SubForumsTableSeeder extends Seeder
 
         $roles = Role::all();
 
-        SubForum::all()->each(function ($user) use ($roles) {
-            $user->roles()->attach(
-                $roles->random(rand(1, 3))->pluck('id')->toArray()
-            );
-        });
+        SubForum::all()->each(
+            function ($user) use ($roles) {
+                $user->roles()->attach(
+                    $roles->random(rand(1, 3))->pluck('id')->toArray()
+                );
+            }
+        );
     }
 }
